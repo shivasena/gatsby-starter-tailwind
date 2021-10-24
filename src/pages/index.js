67f08387be15cbf99fcd8d63836b2fbc26235fdb
shivasena <mika.smith@gmail.com>
2021-10-24 21:16:59 +0300

    upd index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1 className="text-lg font-bold">Hello world!</h1>
+      <div className="light">
+        This is Preconfigured Tailwind CSS for GatsbyJS
+      </div>
+    </>
+  )
 }
